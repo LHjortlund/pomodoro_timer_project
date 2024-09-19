1c71c57ebe1cb31bce3804b81bb4945ec23d5a32
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-09-19 10:51:54 +0200

    added countdown mechanism

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    coun
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
-# import time
-#
-# count = 5
-# while True:
-#     time.sleep(1)
-#     count -= 1
-
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -32,16 +32,18 @@ window.config(padx=100, pady=50, bg = YELLOW)
 
 window.after(1000,)
 
+#timer label
+timer_label = tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+timer_label.grid(column=1, row=0)
+
 #canvas widget
 canvas = Canvas(window, width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100,130, text="00:00" , fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100,130, text="00:00" , fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-#timer label
-timer_label = tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
-timer_label.grid(column=1, row=0)
+count_down(5)
 
 #button der kalder start
 start_button = tkinter.Button(text="Start", highlightthickness=0)
